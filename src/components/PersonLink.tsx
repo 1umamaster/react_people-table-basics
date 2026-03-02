33abf3b9e71c776c6019814866f3a27c8f9b8482
Oleh Lukash <138055276+1umamaster@users.noreply.github.com>
2026-03-02 18:53:57 +0200

    Update PersonLink.tsx

--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const PersonLink: React.FC<Props> = ({ person }) => {
   return (
     <Link
-      to={`#/people/${person.slug}`}
+      to={`/people/${person.slug}`}
       className={classNames({
         'has-text-danger': person.sex === 'f',
       })}
